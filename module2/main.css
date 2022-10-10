33f99fec9a48a98332cce59a299004191e85ec3e
Yahia <100961050+YahiaXXX@users.noreply.github.com>
2022-10-10 12:53:54 +0200

    added

---
+++ module2/main.css
@@ -0,0 +1,75 @@
+* {
+  box-sizing: border-box;
+}
+
+html {
+  background-color: rgb(226, 226, 74);
+}
+
+section {
+  border: 1px white solid;
+  width: 33.33%;
+  float: left;
+  position: relative;
+  /* margin: 1%; */
+
+  padding: 10px;
+  padding-top: 60px;
+  color: white;
+  background-color: rgb(0, 0, 0);
+}
+p {
+  text-align: center;
+}
+
+h1 {
+  text-align: center;
+  padding-top: 5%;
+  padding-bottom: 5%;
+}
+
+section > span {
+  background-color: rgb(109, 109, 109);
+}
+
+span p.title {
+  font-weight: bold;
+}
+
+span {
+  border: 1px solid black;
+  position: absolute;
+  width: 35%;
+  height: 30%;
+  top: 0;
+  bottom: 0;
+  right: 0;
+}
+
+/* Media Queires */
+/* Destop */
+/* 992px */
+
+/* Tablet */
+/* 768px and 991px */
+@media (min-width: 768px) and (max-width: 991px) {
+  section.first {
+    width: 50%;
+  }
+
+  section.second {
+    width: 50%;
+  }
+  section.third {
+    width: 100%;
+    /* margin-top: 20px; */
+  }
+}
+
+/* mobile */
+/* max-width 767px */
+@media (max-width: 767px) {
+  section {
+    width: 100%;
+  }
+}
---
+++ module3/README.md
@@ -0,0 +1,2 @@
+# module3-solution
+Peer-graded Assignment: Module 3 Coding Assignment
